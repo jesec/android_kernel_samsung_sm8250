2209722c259a89e190fc456d58e452c9941686d3
Chris Wulff <crwulff@gmail.com>
2020-03-14 12:54:49 -0400

    ALSA: usb-audio: Create a registration quirk for Kingston HyperX Amp (0951:16d8)
    [ Upstream commit 55f7326170d9e83e2d828591938e1101982a679c ]
    
    Create a quirk that allows special processing and/or
    skipping the call to snd_card_register.
    
    For HyperX AMP, which uses two interfaces, but only has
    a capture stream in the second, this allows the capture
    stream to merge with the first PCM.
    
    Signed-off-by: Chris Wulff <crwulff@gmail.com>
    Link: https://lore.kernel.org/r/20200314165449.4086-3-crwulff@gmail.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -751,11 +751,14 @@ static int usb_audio_probe(struct usb_interface *intf,
 
 	set_usb_audio_cardnum(chip->card->number, 0, 1);
 
-	/* we are allowed to call snd_card_register() many times */
-	err = snd_card_register(chip->card);
-	if (err < 0)
-		goto __error;
-	pr_info("%s : card %d is registered.\n", __func__, chip->card->number);
+	/* we are allowed to call snd_card_register() many times, but first
+	 * check to see if a device needs to skip it or do anything special
+	 */
+	if (!snd_usb_registration_quirk(chip, ifnum)) {
+		err = snd_card_register(chip->card);
+		if (err < 0)
+			goto __error;
+	}
 
 	usb_chip[chip->index] = chip;
 	chip->num_interfaces++;
@@ -766,7 +769,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 	mutex_unlock(&register_mutex);
 	return 0;
 
- __error:
+ __error
 	pr_info("%s : card probe fail.\n", __func__);
 	if (chip) {
 		set_usb_audio_cardnum(chip->card->number, 0, 0);
