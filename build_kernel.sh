1ce71586342cb447ee938ba0d70f7e56a464cd53
Jesse Chan <jc@linux.com>
2020-03-25 18:04:17 +0000

    sm8250: build: use arguments, make dtb and dtbo images
    ./build_kernel.sh ${MODEL} ${REGION} ${CARRIER}

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+CHIPSET_NAME=kona
+MODEL=$1
+REGION=$2
+CARRIER=$3
+
 export ARCH=arm64
 mkdir out
 
@@ -8,7 +13,13 @@ KERNEL_LLVM_BIN=$ANDROID_TOP/prebuilts/clang/host/linux-x86/clang-r353983d/bin/c
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_chn_openx_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
-#cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/${MODEL}_${REGION}_${CARRIER}_defconfig || exit 1
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
+
+cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
+
+DTS_DIR=$(pwd)/out/arch/$ARCH/boot/dts
+DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-${MODEL}-${REGION}-overlay*.dtbo)
+
+cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/out/dtb.img
+$(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 ${DTBO_FILES}
