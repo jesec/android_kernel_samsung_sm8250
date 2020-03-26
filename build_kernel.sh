a6f52bfad0eb487d07d50d352711e2db4cf5f733
Jesse Chan <jc@linux.com>
2020-03-26 15:39:55 +0000

    sm8250: build: switch to LLVM linker and tools

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,13 +8,15 @@ CARRIER=$3
 export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=$ANDROID_TOP/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$ANDROID_TOP/prebuilts/clang/host/linux-x86/clang-r353983d/bin/clang
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
+KERNEL_LLVM_BIN=$ANDROID_TOP/prebuilts/clang/host/linux-x86/clang-r353983d/bin
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/${MODEL}_${REGION}_${CARRIER}_defconfig || exit 1
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
+if [[ $4 == "lto" ]]; then KERNEL_MAKE_ENV="$KERNEL_MAKE_ENV VARIANT_DEFCONFIG=vendor/lto_defconfig"; fi
+
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN/clang LD=$KERNEL_LLVM_BIN/ld.lld NM=$KERNEL_LLVM_BIN/llvm-nm OBJCOPY=$KERNEL_LLVM_BIN/llvm-objcopy CLANG_TRIPLE=$CLANG_TRIPLE vendor/${MODEL}_${REGION}_${CARRIER}_defconfig || exit 1
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN/clang LD=$KERNEL_LLVM_BIN/ld.lld NM=$KERNEL_LLVM_BIN/llvm-nm OBJCOPY=$KERNEL_LLVM_BIN/llvm-objcopy CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
 
 cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
 
