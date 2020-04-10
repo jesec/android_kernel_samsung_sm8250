304ea056c14afc6722d281870e8b99185de74c6e
Jesse Chan <jc@linux.com>
2020-04-10 23:11:37 +0800

    sm8250: set up Github Actions for STAGING

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,14 +9,13 @@ export ARCH=arm64
 mkdir out
 
 BUILD_CROSS_COMPILE=aarch64-linux-gnu-
-KERNEL_LLVM_BIN=$ANDROID_TOP/prebuilts/clang/host/linux-x86/clang-r377782c/bin
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 if [[ $4 == "lto" ]]; then KERNEL_MAKE_ENV="$KERNEL_MAKE_ENV VARIANT_DEFCONFIG=vendor/lto_defconfig"; fi
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN/clang LD=$KERNEL_LLVM_BIN/ld.lld NM=$KERNEL_LLVM_BIN/llvm-nm AR=$KERNEL_LLVM_BIN/llvm-ar OBJCOPY=$KERNEL_LLVM_BIN/llvm-objcopy CLANG_TRIPLE=$CLANG_TRIPLE vendor/${MODEL}_${REGION}_${CARRIER}_defconfig || exit 1
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN/clang LD=$KERNEL_LLVM_BIN/ld.lld NM=$KERNEL_LLVM_BIN/llvm-nm AR=$KERNEL_LLVM_BIN/llvm-ar OBJCOPY=$KERNEL_LLVM_BIN/llvm-objcopy CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=${KERNEL_LLVM_BIN}clang LD=${KERNEL_LLVM_BIN}ld.lld NM=${KERNEL_LLVM_BIN}llvm-nm AR=${KERNEL_LLVM_BIN}llvm-ar OBJCOPY=${KERNEL_LLVM_BIN}llvm-objcopy OBJDUMP=${KERNEL_LLVM_BIN}llvm-objdump CLANG_TRIPLE=$CLANG_TRIPLE vendor/${MODEL}_${REGION}_${CARRIER}_defconfig || exit 1
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=${KERNEL_LLVM_BIN}clang LD=${KERNEL_LLVM_BIN}ld.lld NM=${KERNEL_LLVM_BIN}llvm-nm AR=${KERNEL_LLVM_BIN}llvm-ar OBJCOPY=${KERNEL_LLVM_BIN}llvm-objcopy OBJDUMP=${KERNEL_LLVM_BIN}llvm-objdump CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
 
 cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
 
