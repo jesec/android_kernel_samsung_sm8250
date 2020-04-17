2978624d9ed8de42dd8002e5a8324f6936c728e9
Jesse Chan <jc@linux.com>
2020-04-17 06:31:24 +0000

    sm8250: set up kernel build script
    usage: ./build_kernel.sh <variant> [release version]
    
    Change-Id: Ib274341a88bae633ca434f20d1465960102f9591

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
+CHIPSET_NAME=kona
+VARIANT=$1
+
 export ARCH=arm64
-mkdir out
+mkdir -p out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/8.0/bin/clang
+DTS_DIR=$(pwd)/out/arch/$ARCH/boot/dts
+DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo 2>/dev/null)
+rm -rf ${DTS_DIR}/vendor/qcom/*.dtb ${DTBO_FILES}
+
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
+BUILD_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV="VARIANT_DEFCONFIG=vendor/variant_$1_defconfig"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_chn_openx_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+case $2 in
+  savedefconfig)
+    SAVE_DEFCONFIG="savedefconfig"
+    ;;
+
+  "")
+    ;;
+
+  *)
+    KERNEL_MAKE_ENV="$KERNEL_MAKE_ENV LOCALVERSION=-$2 DEBUG_DEFCONFIG=vendor/release_defconfig"
+    ;;
+esac
+
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_chn_openx_defconfig || exit 1
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE $SAVE_DEFCONFIG || exit 1
+
+if [[ ! -z "$SAVE_DEFCONFIG" ]]; then
+cp $(pwd)/out/defconfig $(pwd)/arch/$ARCH/configs/vendor/x1q_chn_openx_defconfig
+exit 0
+fi
+
+cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
+
+DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo)
+
+cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/out/dtb.img
+$(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 ${DTBO_FILES}
