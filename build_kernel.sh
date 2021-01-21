972094971c7fac9398c040e027c75e46bd47bc04
Jesse Chan <jc@linux.com>
2021-01-21 18:55:44 +0800

    sm8250: set up kernel build script
    usage: ./build_kernel.sh <variant> [release version]
    
    ./build_kernel.sh x1q chn_openx 1.0
    
    Change-Id: I8d7dba0408b5ca21adb1a757eaa5ce5d80882f75

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+CHIPSET_NAME=kona
+VARIANT=$1
+REGION=$2
+
 export ARCH=arm64
-mkdir out
+mkdir -p out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+DTS_DIR=$(pwd)/out/arch/$ARCH/boot/dts
+DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo 2>/dev/null)
+rm -f ${DTS_DIR}/vendor/qcom/*.dtb ${DTBO_FILES}
+
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
+BUILD_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_chn_openx_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+KERNEL_MAKE_ENV="ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE"
+KERNEL_MAKE_ENV="$KERNEL_MAKE_ENV VARIANT_DEFCONFIG=vendor/$1/kona_sec_$1_$2_defconfig"
+
+case $3 in
+    "")
+    ;;
+
+    *)
+        KERNEL_MAKE_ENV="$KERNEL_MAKE_ENV LOCALVERSION=-$3 DEBUG_DEFCONFIG=vendor/release_defconfig"
+    ;;
+esac
+
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV vendor/kona_sec_defconfig || exit 1
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV || exit 1
+
+cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
+
+DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo)
+
+cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/out/dtb.img
+$(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 ${DTBO_FILES}
